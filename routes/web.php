5acc046f7f56f5fa543c3cac12497fe049763a50
kk123nnn <32289006+kk123nnn@users.noreply.github.com>
2017-09-27 16:09:33 +1300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
